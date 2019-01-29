718498fd14e6fcfbdee2ac9af1d05028e529b97e
pansara <46087442+pansara@users.noreply.github.com>
2019-01-29 11:01:35 -0500

    Add files via upload

---
+++ ApplePicker.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ApplePicker : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public GameObject basketPrefab;
+    public int numBaskets = 3;
+    public float basketBottomY = -14f;
+    public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        basketList = new List<GameObject>();
+        for (int i = 0; i < numBaskets; i++)
+        {
+            GameObject tBasketGo = Instantiate<GameObject>(basketPrefab);
+            Vector3 pos = Vector3.zero;
+            pos.y = basketBottomY + (basketSpacingY * i);
+            tBasketGo.transform.position = pos;
+            basketList.Add(tBasketGo);
+        }
+    }
+
+    public void AppleDestroyed()
+    {
+        //Destroy all the falling apples
+        GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+        foreach (GameObject tGO in tAppleArray)
+        {
+            Destroy(tGO);
+        }
+
+        //Destroy one of the baskets 
+        //Get the index of last basket in basketList
+        int basketIndex = basketList.Count - 1;
+        //Get a reference to that Basket GameObject
+        GameObject tBasketGO = basketList[basketIndex];
+        //Remove the Basket from the list and destroy the GameObject
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
+
+        //If there are no Baskets left, restart the game
+        if(basketList.Count == 0)
+        {
+            //SceneManager.LoadScene("_Scene_0");
+            Application.LoadLevel("gameover");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (numBaskets <= 0)
+        {
+            
+        }
+    }
+}
---
+++ AppleTree.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    //Prefab for instantiating objects
+    public GameObject applePrefab;
+
+    //Speed at which the AppleTree moves
+    public float speed = 1f;
+
+    //Distance where AppleTree turns around
+    public float leftAndRightEdge = 10f;
+
+    //Chance that the AppleTree will change directions
+    public float chanceToChangeDirections = 0.1f;
+
+    //Rate at which Apples will be instantiated
+    public float secondsBetweenAppleDrops = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Dropping apple every second
+        Invoke("DropApple", 2f);
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        //Changing Direction
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);   //Move Right
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);  //Move Left
+        }
+    }
+
+    //Chaging Direction is now time-based because of FixedUpdate()
+    void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;    //Change direction
+        }
+    }
+}
